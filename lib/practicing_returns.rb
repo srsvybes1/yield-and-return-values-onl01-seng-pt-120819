ee26943b70da3bb9481fefd82651d0fd418c6911
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-22 07:43:02 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,13 @@
 require 'pry'
-
+binding.pry
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-   yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
-end
+  collection
+  end
 
-binding.pry
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+(["Tom", "Jim"]) { |name| puts "Hi, #{name}" }
